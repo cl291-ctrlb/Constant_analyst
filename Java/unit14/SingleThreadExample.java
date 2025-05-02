1bbebb09412d60768bfd99f4e0aeb676fb883cbb
cl291-ctrlb <constantlearning291@gmail.com>
2025-05-02 14:32:28 +0530

    Update SingleThreadExample.java

--- Java/unit14/SingleThreadExample.java
+++ Java/unit14/SingleThreadExample.java
@@ -462,7 +462,43 @@ public class ThreadProrityExample{
 //SYNCHRONIZING THREADS
 //Synchronized Method
 //Synchronized Statement
+class Counter{
+  private int count = 0;
+      // Synchronized method
+  public synchronized void increment(){
+    count++;
+  }
+  public int getCount(){
+    return count;
+  }
+}
+public class SynchronizedMethodExample {
+  public static void main(String[] args){
+      Counter counter = new Counter();
+// Creating multiple threads to increment the counter
+    Thread thread1 = new Thread(() ->{
+      for (int i = 0; i<1000; i++){
+        counter.increment();
+      }
+    });
+    Thread thread2 = new Thread(() -> {
+      for(int i = 0; i<1000; i++){
+        counter.increment();
+      }
+    });
+    thread1.start();
+    thread2.start();
 
+    try {
+      thread1.join();
+      thread2.join();
+    }catch (InterruptedException e){
+      e.printStackTrace();
+    }
+    System.out.println("final count"+counter.getCount());
+  }
+
+}
 /*INTERTHREAD COMMUNICATION
 Often, threads will need to coordinate between themselves, especially when the output of one thread is
 used by another thread. One way of coordinating threads is to use the wait(), notify(), and notifyAll()
